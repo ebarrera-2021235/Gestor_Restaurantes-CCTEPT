6a5665ad5cb00f9f95456473b867b15836916d43
ebarrera-2021235 <ebarrera-2021235@kinal.edu.gt>
2026-02-15 10:25:03 -0600

    feat(domain): Definir roles permitidos por el sistema

--- authentication-service/user-service/src/UserService.Domain/Constants/RoleConstants.cs
+++ authentication-service/user-service/src/UserService.Domain/Constants/RoleConstants.cs
@@ -4,5 +4,9 @@ namespace UserService.Domain.Constants;
 
 public class RoleConstants
 {
+    public const string USER_ROLE = "USER_ROLE";
+    public const string ADMIN_ROLE = "ADMIN_ROLE";
+
+    public static readonly string [] AllowedRoles = [USER_ROLE, ADMIN_ROLE];
     
 }
