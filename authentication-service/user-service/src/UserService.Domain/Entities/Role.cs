6789c456f1e7e85d58611f6cb05b772e68c6c01e
atun-2021469 <atun-2021469@kinal.edu.gt>
2026-02-16 10:55:02 -0600

    Feat(devops): Agregar Clases Role y userRole entities

---
+++ authentication-service/user-service/src/UserService.Domain/Entities/Role.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserService.Domain.Entities;
+
+public class Role
+{
+    [Key]
+    [Column("id_rol")]
+    public int IdRol { get; set; }
+
+    [Required(ErrorMessage = "El nombre del rol es obligatorio.")]
+    [MaxLength(35, ErrorMessage = "El nombre del rol no puede exceder los 35 caracteres.")]
+    [Column("nombre_rol")]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(255)]
+    [Column("permisos")]
+    public string? Permisos { get; set; }
+
+    [Column("fecha_creacion")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [Column("fecha_actualizacion")]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    // --- Relaciones ---
+
+    // Permite ver los usuarios asignados vía tabla intermedia
+    public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+
+    // Permite ver los usuarios que tienen este rol como principal (FK directa)
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+}
