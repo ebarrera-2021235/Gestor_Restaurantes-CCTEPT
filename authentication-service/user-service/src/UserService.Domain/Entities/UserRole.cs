8148e9f7eb99cd711e91f184624d53759955c5c0
atun-2021469 <atun-2021469@kinal.edu.gt>
2026-02-15 20:23:38 -0600

    feat (devops): Crear Carpeta de Entities dentro de la entidad Dominios

---
+++ authentication-service/user-service/src/UserService.Domain/Entities/UserRole.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Domain.Entities;
+
+public class UserRole
+{
+    [Key]
+    [MaxLength(16)]
+    public string Id {get; set;} = string.Empty;
+
+    [Key]
+    [MaxLength(16)]
+    public String UserId {get; set;} = string.Empty;
+
+    [Key]
+    [MaxLength(16)]
+    public String RoleId {get; set;} = string.Empty;
+
+
+    public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
+    public DateTime UpdatedAt {get; set;} = DateTime.UtcNow;
+
+    public User User {get;  set;} = null!; 
+
+    public Role Role {get; set;} = null!; 
+}
